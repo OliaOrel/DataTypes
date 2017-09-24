ddc2c2f1965449d9a6f4e182d53d3552f51c3dbc
mille-nium <dianaboloten.db@gmail.com>
2017-09-24 17:10:04 +0300

    Add whitespace

--- JavaScript/2-const-let.js
+++ JavaScript/2-const-let.js
@@ -5,6 +5,8 @@ console.dir({ bubbleEffect });
 var bubbleEffect = 5;
 let scalarVariable = 6;
 const scalarConstant = 7;
+ 
+console.dir(o); 
 
 const o = { scalarVariable, scalarConstant };
 console.dir(o);
@@ -12,4 +14,4 @@ console.dir(o);
 o.scalarVariable = 8;
 scalarVariable = 9;
 
-console.dir(o);
+
